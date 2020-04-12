6ce79170fccb3cf24df0f6a92028433d3e17a412
Aditya Sharma <asharma@AMBITENERGY.COM>
2020-04-12 17:06:16 -0500

    Delete old version scripts

--- find-inactive-vms-from-ad/get-ad-vm-list-v2.ps1
+++
@@ -1,8 +0,0 @@
-﻿$DaysInactive = 90
-$time = (Get-Date).Adddays(-($DaysInactive))
-Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 2000 -resultSetSize $null -Properties Name,DNSHostname,Enabled | select DNSHostName,Enabled | Export-CSV “C:\Temp\HostNames.txt” –NoTypeInformation
-
-Get-Content -Path “C:\Temp\HostNames.txt” | Where-Object {$_ -like '*True*'}
-
-#| Export-CSV “C:\Temp\StaleComps.CSV” –NoTypeInformation
-
--- find-inactive-vms-from-ad/get-ad-vm-list.bash
+++
@@ -1,1 +0,0 @@
-cat stalecomps.csv | awk -F '"' {'print $4,$6'} | grep True
